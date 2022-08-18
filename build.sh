02619b7d2dfaf1930cdc382c7774f64c76b9be74
Abdallah Cheikh <abdallah93.as@gmail.com>
2022-08-19 00:37:25 +0200

    updated build to support the new build environment

--- build.sh
+++ build.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
 echo BUILDING FP_GM.C
-gcc -g fp_gm.c -lm -o fp_gm.out
+
+if [ -e build ]
+then
+	cd build
+else
+	mkdir build
+	cd build
+fi
+
+gcc -I ../libs/include -c ../libs/src/error_check.c ../libs/src/float_transform.c ../libs/src/round.c ../libs/src/parser.c
+ar 	cr libs.a *.o
+gcc -g ../app/fp_gm.c -lm  libs.a -I ../libs/include -o fp_gm.out
