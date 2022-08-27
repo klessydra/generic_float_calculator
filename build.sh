96c3b365dce74a7f2ae0a40a95c02df53c419f2e
Abdallah Cheikh <abdallah93.as@gmail.com>
2022-08-27 15:12:28 +0200

    I enabled the addition of debugging symbols

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ else
 	cd build
 fi
 
-gcc -I ../libs/include -c ../libs/src/error_check.c ../libs/src/float_transform.c ../libs/src/round.c ../libs/src/parser.c ../libs/src/auxiliary.c
+gcc -g -I ../libs/include -c ../libs/src/error_check.c ../libs/src/float_transform.c ../libs/src/round.c ../libs/src/parser.c ../libs/src/auxiliary.c
 ar 	cr libs.a *.o
 gcc -g ../app/fp_gm.c -lm  libs.a -I ../libs/include -o fp_gm.out
