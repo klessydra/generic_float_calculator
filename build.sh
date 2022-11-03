fd8e74f06c4b5efd03a479addf7f25f86f3d33eb
Abdallah Cheikh <abdallah93.as@gmail.com>
2022-11-03 21:11:08 +0100

    create result dir incase its missing

--- build.sh
+++ build.sh
@@ -10,6 +10,11 @@ else
 	cd build
 fi
 
-gcc -g -I ../libs/include -c ../libs/src/error_check.c ../libs/src/float_transform.c ../libs/src/round.c ../libs/src/parser.c ../libs/src/auxiliary.c
+if [ ! -e "../result" ]
+then
+	mkdir result
+fi
+
+gcc -std=c99 -g -I ../libs/include -c ../libs/src/error_check.c ../libs/src/float_transform.c ../libs/src/round.c ../libs/src/parser.c ../libs/src/auxiliary.c
 ar 	cr libs.a *.o
-gcc -g ../app/fp_gm.c -lm  libs.a -I ../libs/include -o fp_gm.out
+gcc -std=c99 -g ../app/fp_gm.c -lm  libs.a -I ../libs/include -o fp_gm.out
